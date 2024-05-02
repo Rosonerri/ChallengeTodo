6d135c44526a1b10a645ca34066d45e3517844b9
rosonerri <gabrieldotajayi4real@gmail.com>
2024-05-02 13:27:58 +0100

    dist Added

--- dist/Index.js
+++ dist/Index.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 console.clear();
 const express_1 = __importDefault(require("express"));
@@ -16,16 +18,16 @@ app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 (0, mainApp_1.mainApp)(app);
 const server = app.listen(port, () => {
-    console.log("App Listening to port on ", port);
-    (0, dbConfig_1.dbConfig)();
+  console.log("App Listening to port on ", port);
+  (0, dbConfig_1.dbConfig)();
 });
 process.on("uncaughtException", (error) => {
-    console.log("uncaughtException: ", error);
-    process.exit(1);
+  console.log("uncaughtException: ", error);
+  process.exit(1);
 });
 process.on("unhandledRejection", (reason) => {
-    console.log("unhandledRejection: ", reason);
-    server.close(() => {
-        process.exit(1);
-    });
+  console.log("unhandledRejection: ", reason);
+  server.close(() => {
+    process.exit(1);
+  });
 });
