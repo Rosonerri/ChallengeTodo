03060dafdb82010554dbe17d909c7b5687c0f413
rosonerri <gabrieldotajayi4real@gmail.com>
2024-05-02 13:14:36 +0100

    dist Added

--- utils/dbConfig.ts
+++ utils/dbConfig.ts
@@ -1,6 +1,8 @@
 import { connect } from "mongoose";
 
-const URL: string = "mongodb://localhost:27017/simpleTodoDB";
+const URL: string =
+  "mongodb+srv://tobez:tobez@cluster0.l4hcy57.mongodb.net/simpleTodoDB?retryWrites=true&w=majority&appName=Cluster0";
+
 export const dbConfig = async () => {
   try {
     return await connect(URL)
